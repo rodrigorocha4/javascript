3baa7e9b893a8cd91fa4d5099bb9c1ddbaa9f2bf
Rodrigo Rocha <rodrigorocha01114@gmail.com>
2023-02-21 15:53:31 -0300

    .

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 100
+
+console.log(`A velocidade do seu carro é ${vel}km/h `)
+if (vel > 60){
+     console.log(`Você ultrapassou a velociade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em pais ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`você é Brasileiro!`)
+} else{
+    console.log(`Você é Estrangeiro!`)
+}

d23c6fcaacce46147b5b7289b4f56a3cd93d3c4d
Rodrigo Rocha <rodrigorocha01114@gmail.com>
2023-03-02 15:18:38 -0300

    .

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+} while (c < 6) 
+
+
+
+
+/*
+
+var c = 1
+while (c < 6) {
+    console.log('Tudo bem?')
+}
+
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
